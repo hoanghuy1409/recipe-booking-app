50e05f9129529260e6be9064897a97434d6ca567
Hoang Huy <hoanghuy.gg96@gmail.com>
2020-01-29 16:01:09 +0700

    routing

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { ShoppingListService } from "./../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.module";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "this is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("meat", 2)]
+    ),
+    new Recipe(
+      "A  recipe",
+      "this is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("buns", 2), new Ingredient("meat", 2)]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
